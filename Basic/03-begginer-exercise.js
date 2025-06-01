09e4324c7c193e32013058748fa57c61ffbf7a15
Paula <pmartingc@gmail.com>
2025-06-01 13:35:45 +0100

    Anadimos varias lecciones más

---
+++ Basic/03-begginer-exercise.js
@@ -0,0 +1,80 @@
+// 1. Escribe un comentario en una línea
+
+// 2. Escribe un comentario de varias Líneas
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivo
+
+let stringVar = "Â¡Hola, JavaScript"
+let numberVar = 37
+let booleanVar = true
+let nullVar = null
+let undefinedVar
+let bigIntVar = BigInt(9007199254740991)
+let symbolVar = Symbol("unique")
+
+// 4. Imprime por consola el valor de todas las variables.
+
+console.log(stringVar)
+console.log(numberVar)
+console.log(booleanVar)
+console.log(nullVar)
+console.log(undefinedVar)
+console.log(bigIntVar)
+console.log(symbolVar)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+
+console.log(typeof stringVar)
+console.log(typeof numberVar)
+console.log(typeof booleanVar)
+console.log(typeof nullVar)
+console.log(typeof undefinedVar)
+console.log(typeof bigIntVar)
+console.log(typeof symbolVar)
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+stringVar = "Â¡AdiÃ³s, JavaScript!"
+numberVar = 38
+booleanVar = false
+nullVar = null
+undefinedVar = undefined
+bigIntVar = BigInt(1234567890123456)
+symbolVar = Symbol("anotherUnique")
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+stringVar = 1000
+numberVar = "Brais Moure"
+booleanVar = "true"
+nullVar = BigInt(1234567890123456)
+undefinedVar = false
+bigIntVar = 3.14159
+symbolVar = null
+
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+
+const constString = "Soy una constante"
+const constNumber = 37
+const constBoolean = false
+const constNull = null
+const constUndefined = undefined
+const constBigInt = BigInt(789654123)
+const constSymbol = Symbol("constantSymbol")
+
+// 9. A continuación modifica los valores de las constantes
+
+// constString = "Nueva constante"
+// constNumber = 38
+// constBoolean = true
+// constNull = "null"
+// constUndefined = null
+// constBigInt = BigInt(987654321)
+// constSymbol = Symbol("newConsatntSymbol")
+
+// 10. Comenta las línea que produzcan algún error al ejecutarse
+
+//Modificado en el ejercicio 9.
