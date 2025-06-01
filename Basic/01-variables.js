122caad4f98c36dd15240d87d7009acb9fdee42e
Paula <pmartingc@gmail.com>
2025-06-01 11:19:50 +0100

    Añadir nuevos archivos

---
+++ Basic/01-variables.js
@@ -0,0 +1,9 @@
+// var
+var helloWorld = "Hello, Javascript"
+helloWorld = "Hola de nuevo, Javascript"
+
+// let
+
+let helloWorld2 = "Hola mundillo"
+
+// const
---
+++ Basic/02-datatype.js
@@ -0,0 +1,35 @@
+// DATOS PRIMITIVOS🙊
+
+// String ( Cadena de texto)
+
+let name = "Paula Margon"
+let alias = "pmargon"
+let email = `pmargon@prueba.com`
+
+// Números (Number)
+let age = 37 //entero
+let height = 1.99 // Decimal
+
+// Booleanos ( Boolean)
+let isTeacher = true
+let isStudent = false
+
+// Undefined
+let undefinedValue
+
+// Null
+let Nullvalue = null
+
+// Symbol
+
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+
+let myBigInt = BigInt(57456878454855498521563485920001458624)
+let myBigInt2 = 888888888888888888888888888888888888888888884555555555555555555555555556n
+
+// Mostrar que tipo de dato tiene
+
+console.log(typeof name)
+console.log(3*3)
