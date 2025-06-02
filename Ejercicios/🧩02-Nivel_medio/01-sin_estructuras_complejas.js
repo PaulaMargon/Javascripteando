b4c013ae0a3facfd097c3e71ff1c38e08b5fea35
PaulaMargon <pmartingc@gmail.com>
2025-06-02 07:20:37 +0100

    Ejercicios 02 nivel medio realizados

--- Ejercicios/🧩02-Nivel_medio/01-sin_estructuras_complejas.js
+++ Ejercicios/🧩02-Nivel_medio/01-sin_estructuras_complejas.js
@@ -42,3 +42,43 @@ if(temperatura > 30){
 }else{
     console.log("Saca la manta❄️")
 }
+
+// ** Sistema de acceso para un club secreto **
+// El usuario tiene que introducir una frase secreta. Si la frase contiene la palabra “draco” y tiene exactamente 14 caracteres, accede. Si falla, rechaza el acceso con una frase divertida.
+
+let frase_secreta = "draco123456789"
+
+if (frase_secreta.includes("draco") && frase_secreta.length === 14){
+  console.log("Acceso concedido, bienvenido al club secreto🐲")
+}else {
+  console.log("Acceso denegado ¿Te has confundido de frase o de club? :P")
+}
+
+// Reto de Palabra Espejo
+// El usuario introduce dos palabras. Si son iguales al derecho y al revés (ej: "reconocer"), felicítalo por encontrar un palíndromo. Si no, dile que lo intente de nuevo con más cuidado.
+
+const fraseUsuario = "oso"
+
+if (fraseUsuario === fraseUsuario.split("").reverse().join("")) {
+  console.log("¡Felicidades! ¡Has encontrado un palíndromo!");
+} else {
+  console.log("¡Inténtalo de nuevo con más cuidado!");
+}
+
+// ** Simulador de selector de clase de RPG**
+// Pide al usuario su nombre y una clase entre "guerrero", "mago" o "ladrón". Según la clase, dale un mensaje personalizado y un objeto inicial:
+// Guerrero: Espada oxidada
+// Mago: Bastón de sabiduría
+// Ladrón: Cuchillo de sombra
+
+let nombre_usuario = "Pedro"
+let clase_rpg = "mago"
+
+if (clase_rpg === "guerrero"){
+  console.log(`Bienvenido ${nombre_usuario} tu clase es ${clase_rpg} y recibes una Espada oxidada para comenzar🤺`)
+} else if ( clase_rpg === "mago" ){
+  console.log(`Bienvenido ${nombre_usuario} tu clase es ${clase_rpg} y recibes un bastón de sabiduría🧙‍♂️`)
+} else {
+  console.log(`Bienvenido ${nombre_usuario} tu clase es ${clase_rpg} y un cuchillo de sombra🥷`)
+
+}
