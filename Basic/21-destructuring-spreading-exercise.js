f336d2eefe958f5a520c046cea8c95c98d063ccd
PaulaMargon <pmartingc@gmail.com>
2025-06-04 18:39:45 +0100

    Destructuración y propagación

---
+++ Basic/21-destructuring-spreading-exercise.js
@@ -0,0 +1,19 @@
+// 1. Usa desestructuraciÃ³n para extraer los dos primeros elementos de un array.
+
+// 2. Usa desestructuraciÃ³n en un array y asigna un valor predeterminado a una variable.
+
+// 3. Usa desestructuraciÃ³n para extraer dos propiedades de un objeto.
+
+// 4. Usa desestructuraciÃ³n para extraer dos propiedades de un objeto y asÃ­gnalas a nuevas variables con nombres diferentes.
+
+// 5. Usa desestructuraciÃ³n para extraer dos propiedades de un objeto anidado.
+
+// 6. Usa propagaciÃ³n para combinar dos arrays en uno nuevo.
+
+// 7. Usa propagaciÃ³n para crear una copia de un array.
+
+// 8. Usa propagaciÃ³n para combinar dos objetos en uno nuevo.
+
+// 9. Usa propagaciÃ³n para crear una copia de un objeto.
+
+// 10. Combina desestructuraciÃ³n y propagaciÃ³n.
