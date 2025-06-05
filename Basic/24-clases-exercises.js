ce8889d3cfd0660559168f7eb679c67b030ccbb8
PaulaMargon <pmartingc@gmail.com>
2025-06-05 07:27:09 +0100

    Clases y herencias

---
+++ Basic/24-clases-exercises.js
@@ -0,0 +1,19 @@
+// 1. Crea una clase que reciba dos propiedades.
+
+// 2. AÃ±ade un mÃ©todo a la clase que utilice las propiedades.
+
+// 3. Muestra los valores de las propiedades e invoca a la funciÃ³n.
+
+// 4. AÃ±ade un mÃ©todo estÃ¡tico a la primera clase.
+
+// 5. Haz uso del mÃ©todo estÃ¡tico.
+
+// 6. Crea una clase que haga uso de herencia.
+
+// 7. Crea una clase que haga uso de getters y setters.
+
+// 8. Modifica la clase con getters y setters para que use propiedades privadas.
+
+// 9. Utiliza los get y set y muestra sus valores.
+
+// 10. Sobrescribe un mÃ©todo de una clase que utilice herencia.
